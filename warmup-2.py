c167793ecdec4abbc20c116192d7548aa2e32bd4
Walter Lam <54109442+walterlam@users.noreply.github.com>
2020-01-29 10:29:24 +0800

    Update warmup-2.py

--- warmup-2.py
+++ warmup-2.py
@@ -66,4 +66,85 @@ def string_splosion(str):
             result = result + str[j]
     return result
 
+def last2(str):
+    '''
+    Given a string, return the count of the number of times that a substring 
+    length 2 appears in the string and also as the last 2 chars of the string,
+    so "hixxxhi" yields 1 (we won't count the end substring).
+    
+    last2('hixxhi') → 1
+    last2('xaxxaxaxx') → 1
+    last2('axxxaaxx') → 2
+    '''
+    if len(str) < 2:
+        return 0
+    else:
+        count = 0
+        check = str[-2:]
+        for i in range(len(str)-2):
+            if str[i:i+2] == check:
+                count +=1
+        return count
 
+def array_count9(nums):
+    '''
+    Given an array of ints, return the number of 9's in the array.
+    
+    array_count9([1, 2, 9]) → 1
+    array_count9([1, 9, 9]) → 2
+    array_count9([1, 9, 9, 3, 9]) → 3
+    '''
+    count = 0
+    for num in nums:
+        if num == 9:
+            count += 1
+    return count
+
+def array_front9(nums):
+    '''
+    Given an array of ints, return True if one of the first 4 elements in the 
+    array is a 9. The array length may be less than 4.
+    
+    array_front9([1, 2, 9, 3, 4]) → True
+    array_front9([1, 2, 3, 4, 9]) → False
+    array_front9([1, 2, 3, 4, 5]) → False
+    '''
+    checknum = min(4,len(nums))
+    for i in range(checknum):
+        if nums[i] == 9:
+            return True
+    return False
+
+def array123(nums):
+    '''
+    Given an array of ints, return True if the sequence of numbers 1, 2, 3 
+    appears in the array somewhere.
+    
+    array123([1, 1, 2, 3, 1]) → True
+    array123([1, 1, 2, 4, 1]) → False
+    array123([1, 1, 2, 1, 2, 3]) → True
+    '''
+    if len(nums) < 3:
+        return False
+    for i in range(len(nums)-2):
+        if nums[i] == 1 and nums[i+1] == 2 and nums[i+2] == 3:
+            return True
+    return False
+
+def string_match(a, b):
+    '''
+    Given 2 strings, a and b, return the number of the positions where they 
+    contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, 
+    since the "xx", "aa", and "az" substrings appear in the same place in 
+    both strings.
+    
+    string_match('xxcaazz', 'xxbaaz') → 3
+    string_match('abc', 'abc') → 2
+    string_match('abc', 'axc') → 0
+    '''
+    check = min(len(a),len(b))
+    count = 0
+    for i in range(check-1):
+        if a[i] == b[i] and a[i+1] == b[i+1]:
+            count += 1
+    return count
