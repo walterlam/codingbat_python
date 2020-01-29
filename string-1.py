232fb8e70e2c2aa504800c84282a0acd8aa380bb
Walter Lam <54109442+walterlam@users.noreply.github.com>
2020-01-29 10:58:44 +0800

    Update string-1.py

--- string-1.py
+++ string-1.py
@@ -36,7 +36,7 @@ def make_out_word(out, word):
     '''
     Given an "out" string length 4, such as "<<>>", and a word, return a 
     new string where the word is in the middle of the out string, 
-    e.g. "<<word>>".
+    e.g. "<<word>>". 
     
     make_out_word('<<>>', 'Yay') → '<<Yay>>'
     make_out_word('<<>>', 'WooHoo') → '<<WooHoo>>'
